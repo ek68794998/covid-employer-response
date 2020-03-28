b6b29df200d2cbfae640674f7b95d66ae90c82ea
Eric <eric.kumlin@gmail.com>
2020-03-27 23:03:15 -0700

    Add city name to search filter

--- src/web/views/EmployerList/EmployerListSearchFilter.ts
+++ src/web/views/EmployerList/EmployerListSearchFilter.ts
@@ -4,6 +4,11 @@ export class EmployerListSearchFilter {
 	public text: string = "";
 
 	public static isMatch(f: EmployerListSearchFilter, e: EmployerRecord): boolean {
-		return e.name.toLowerCase().indexOf(f.text.toLowerCase()) >= 0;
+		const fieldsToSearch: string[] = [
+			e.name.toLowerCase(),
+			e.location.city.toLowerCase(),
+		];
+
+		return fieldsToSearch.some((field: string) => field.indexOf(f.text.toLowerCase()) >= 0);
 	}
 }
