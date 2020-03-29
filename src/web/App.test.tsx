38a833739bcc4b887b66992588dc16837c604753
Eric <eric.kumlin@gmail.com>
2020-03-28 21:53:08 -0700

    Add Yarn CI build (#4)
    * Add Yarn CI build
    * Fix app test not having a provider

--- src/web/App.test.tsx
+++ src/web/App.test.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import { AnyAction, Store } from "redux";
 
 import App from "./App";
 
-import { MemoryRouter } from "react-router-dom";
+import configureStore from "./state/configureStore";
 
 describe("<App />", () => {
 	test("renders without exploding", () => {
 		const div: HTMLDivElement = document.createElement("div");
+		const store: Store<{}, AnyAction> = configureStore({});
 
 		render(
-			<MemoryRouter>
-				<App />
-			</MemoryRouter>,
+			<Provider store={store}>
+				<MemoryRouter>
+					<App />
+				</MemoryRouter>
+			</Provider>,
 			div,
 		);
 	});
