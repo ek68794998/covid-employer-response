43e199738f028269bb42914dbfa4eb37f8c98af7
Eric <eric.kumlin@gmail.com>
2020-04-06 19:26:39 -0700

    Revert some unwanted changes

--- src/api/controllers/EmployersController.ts
+++ src/api/controllers/EmployersController.ts
@@ -9,8 +9,6 @@ import { RoutedControllerBase } from "./RoutedControllerBase";
 class EmployersController extends RoutedControllerBase {
 	public static readonly SUBPATH: string = "employers";
 
-	private static readonly MAX_RESULTS_GET_EMPLOYERS: number = 10;
-
 	private readonly recordLoader: EmployerRecordLoader = new EmployerRecordLoader(process.env.RAZZLE_PUBLIC_DIR || "/", "employers");
 
 	private employers: EmployerRecord[] = [];
@@ -19,10 +17,6 @@ class EmployersController extends RoutedControllerBase {
 		if (!req.header("if-none-match") || this.employers.length === 0) {
 			this.employers = await this.recordLoader.loadAllAsync();
 		}
-
-		const IsDetailed: boolean = true;
-		const skipParam: string = req.param("$skip", "0");
-		const topParam: string = req.param("$top", EmployersController.MAX_RESULTS_GET_EMPLOYERS);
 
 		res.setHeader("content-type", "application/json");
 		res.send(this.employers);
