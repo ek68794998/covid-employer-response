c86654141e1e4123e48742ad729b0f793fcbb3ed
Eric <eric.kumlin@gmail.com>
2020-04-08 23:51:23 -0700

    Add industry API endpoint

---
+++ src/api/controllers/IndustriesController.ts
@@ -0,0 +1,49 @@
+import express from "express";
+
+import { EmployerRecord } from "../../common/EmployerRecord";
+import { HttpRequestHeaders } from "../../common/http/HttpRequestHeaders";
+import { HttpResponseHeaders } from "../../common/http/HttpResponseHeaders";
+import { MimeTypes } from "../../common/MimeTypes";
+import { DataFileLoaderOptions } from "../storage/DataFileLoaderOptions";
+
+import { EmployerRecordLoader } from "../storage/EmployerRecordLoader";
+
+import { RoutedControllerBase } from "./RoutedControllerBase";
+
+class EmployersController extends RoutedControllerBase {
+	public static readonly SUBPATH: string = "industries";
+
+	private readonly recordLoader: EmployerRecordLoader;
+
+	public constructor(recordLoader: EmployerRecordLoader) {
+		super();
+
+		this.recordLoader = recordLoader;
+	}
+
+	public async getIndustries(req: express.Request, res: express.Response): Promise<void> {
+		const loaderOptions: DataFileLoaderOptions = {
+			bypassCache: !req.header(HttpRequestHeaders.IF_NONE_MATCH),
+		};
+
+		const employers: EmployerRecord[] = await this.recordLoader.getAllAsync(loaderOptions);
+		const industries: { [key: string]: boolean } = {};
+
+		for (const employer of Object.values(employers)) {
+			if (!employer.industries) {
+				continue;
+			}
+
+			employer.industries.forEach((i: string) => industries[i] = true);
+		}
+
+		res.setHeader(HttpResponseHeaders.CONTENT_TYPE, MimeTypes.APPLICATION_JSON);
+		res.send(Object.keys(industries));
+	}
+
+	protected initializeRoutes(): void {
+		this.router.get(`/${EmployersController.SUBPATH}`, this.getIndustries.bind(this));
+	}
+}
+
+export default EmployersController;
