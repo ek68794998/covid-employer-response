299ed5927089fe26e27bfdeb5047a3cc9a7e27e2
Eric <eric.kumlin@gmail.com>
2020-04-19 22:09:53 -0700

    Fix union type linting

--- src/common/EmployerRating.ts
+++ src/common/EmployerRating.ts
@@ -1,7 +1,9 @@
-export const EmployerRatingValues: string[] = [
+// For maintainability of union types, do not require a typedef.
+// eslint-disable-next-line
+export const EmployerRatingValues = [
 	"good",
 	"fair",
 	"poor",
-];
+] as const;
 
 export type EmployerRating = typeof EmployerRatingValues[number];
