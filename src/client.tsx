10a48bde90ba50a7c312cc609c722d2db7a095c3
Eric <eric.kumlin@gmail.com>
2021-01-22 10:42:03 -0800

    Enable LAN testing

--- src/client.tsx
+++ src/client.tsx
@@ -11,9 +11,10 @@ import { BrowserRouter } from "react-router-dom";
 import { AnyAction, Store } from "redux";
 
 import App from "./web/App";
+import { AppState } from "./web/state/AppState";
 import configureStore from "./web/state/configureStore";
 
-const store: Store<{}, AnyAction> = configureStore((window as any).__PRELOADED_STATE__);
+const store: Store<AppState, AnyAction> = configureStore((window as any).__PRELOADED_STATE__);
 
 hydrate(
 	<Provider store={store}>
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/*
+  Imports for hot loading have to be done through require().
+*/
+
 /* istanbul ignore file */
 /*
   No test coverage since testing express startup is not really feasible.
@@ -5,31 +10,50 @@
 */
 
 import express from "express";
+import * as http from "http";
+import { ReactText } from "react";
 
-// this require is necessary for server HMR to recover from error
-// tslint:disable-next-line:no-var-requires
+const onProcessWarning = (notice: Error): void => {
+	console.warn(notice.stack);
+};
+
+process.on("warning", onProcessWarning);
+
 let app: any = require("./server").default;
 
 if (module.hot) {
-	module.hot.accept("./server", () => {
-		console.log("🔁  HMR Reloading `./server`...");
+	module.hot.accept([ "./server" ], (data: ReactText[]): void => {
+		console.log(`🔁 HMR Reloading '${data.join("', '")}'...`);
 		try {
 			app = require("./server").default;
 		} catch (error) {
 			console.error(error);
 		}
 	});
-	console.info("✅  Server-side HMR Enabled!");
+
+	console.info("✅ Server-side HMR Enabled!");
 }
 
-const port: string = process.env.PORT || "3000";
+const host: string = process.env.HOST || "localhost";
+const port: number = parseInt(process.env.PORT || "3000", 10);
 
-export default express()
-	.use((req, res) => app.handle(req, res))
-	.listen(port, (err: Error) => {
-		if (err) {
+const expressServer: express.Express =
+	express().use((req: express.Request, res: express.Response): void => app.handle(req, res));
+
+const httpServer: http.Server =
+	http.createServer(expressServer)
+		.on("error", (err: Error): void => {
 			console.error(err);
-			return;
-		}
-		console.log(`> Started on port ${port} ( http://localhost:${port}/ )`);
+		});
+
+if (host === "localhost") {
+	httpServer.listen(port, (): void => {
+		console.log(`> Started on local host. ( http://localhost:${port}/ )`);
 	});
+} else {
+	httpServer.listen(port, host, (): void => {
+		console.log(`> Started on custom host. ( http://${host}:${port}/ )`);
+	});
+}
+
+export default httpServer;
